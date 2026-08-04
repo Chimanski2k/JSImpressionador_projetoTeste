67dc4cee91da8e35509269e110d28e3967213201
Jhonata <jhonatacg2000@gmail.com>
2026-08-04 13:54:21 -0300

    teste

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ import { turboLogger } from './turboLogger.js';
 
 turboLogger('Estou aprendendo o fluxo do Git', 'black');
 console.error('ERRO?');
-
-console.log('aaaaa');
--- turboLogger.js
+++ turboLogger.js
@@ -1,5 +1,4 @@
 export const turboLogger = (message, textColor = 'blue') => {
   const textStyle = `color: ${textColor}`;
   console.log(`%c${message}`, textStyle);
-  // teste pra git pull
 };
