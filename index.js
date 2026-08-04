f0a5660dc8a280149b07ff8089bad49944d1fd79
Jhonata <jhonatacg2000@gmail.com>
2026-08-04 16:25:50 -0300

    Ataulizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { turboLogger } from './turboLogger.js';
 
-turboLogger('Estou aprendendo o fluxo do Git', 'black');
-console.error('ERRO?');
+turboLogger('Estou aprendendo o fluxo do Git', 'blue');
+turboLogger(
+  'Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema',
+  'green'
+);
